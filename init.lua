2242451833499f5aa1d35132c737a352599f338d
Jordan Irwin <antumdeluge@gmail.com>
2026-06-05 15:03:27 -0700

    Move TODO list into README

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
 dofile(core.get_modpath("rocket") .. "/rocket.lua")
 dofile(core.get_modpath("rocket") .. "/rover.lua")
-
---To do:
---make other_worlds compatible
---fix rover
---make rover craftable
---make new version downloadable
---update forum topic
